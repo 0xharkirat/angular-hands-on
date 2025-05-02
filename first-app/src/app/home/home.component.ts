63f4afd15976a80ff734baa876838173eb44ca6d
Harkirat Singh <sandhukirat23@gmail.com>
2025-05-02 14:19:25 +1000

    create an interface - HousingLocation

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -18,5 +20,18 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
 
 }
